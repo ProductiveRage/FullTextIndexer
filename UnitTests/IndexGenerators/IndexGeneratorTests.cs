fc0eae1d9544db0abad23d340b11ffa4eccfe601
dan <742348+ProductiveRage@users.noreply.github.com>
2012-03-03 16:58:17 +0000

    Added UnitTests project with only a single straight-down-the-middle test (so far)

---
+++ UnitTests/IndexGenerators/IndexGeneratorTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Common.Lists;
+using FullTextIndexer.IndexGenerators;
+using FullTextIndexer.TokenBreaking;
+using Xunit;
+
+namespace UnitTests.IndexGenerators
+{
+    public class IndexGeneratorTests
+    {
+        [Fact]
+        public void SingleProductWithSingleWordName()
+        {
+            var indexGenerator = new IndexGenerator<Product, int>(
+                p => p.Key,
+                new IntEqualityComparer(),
+                p => p.Name,
+                StringComparer.InvariantCultureIgnoreCase,
+                new WhiteSpaceTokenBreaker(new NoActionTokenBreaker()),
+                (token, occurenceCount) => occurenceCount
+            );
+            var index = indexGenerator.Generate(new NonNullImmutableList<Product>(new[]
+            {
+                new Product() { Key = 1, Name = "Product" }
+            }));
+
+            var expected = new NonNullImmutableList<WeightedEntry<int>>(new[]
+            {
+                new WeightedEntry<int>(1, 1f)
+            });
+            EnsureIndexDataMatchesExpectations(
+                expected,
+                index.GetMatches("Product")
+            );
+        }
+
+        /// <summary>
+        /// This will throw an exception if the contents of expected do not match that of actual (or if either reference is null)
+        /// </summary>
+        private void EnsureIndexDataMatchesExpectations(NonNullImmutableList<WeightedEntry<int>> expected, NonNullImmutableList<WeightedEntry<int>> actual)
+        {
+            if (expected == null)
+                throw new ArgumentNullException("expected");
+            if (actual == null)
+                throw new ArgumentNullException("actual");
+
+            if (expected.Count != actual.Count)
+                throw new ArgumentException("expected.Count does not match actual.Count");
+
+            Comparison<WeightedEntry<int>> sorter = (x, y) =>
+            {
+                var keyComparison = x.Key.CompareTo(y);
+                if (keyComparison != 0)
+                    return keyComparison;
+                return x.Weight.CompareTo(y.Weight);
+            };
+            expected = expected.Sort(sorter);
+            actual = actual.Sort(sorter);
+            
+            for (var index = 0; index < expected.Count; index++)
+            {
+                if ((expected[index].Key != actual[index].Key) || (expected[index].Weight != actual[index].Weight))
+                    throw new ArgumentException("expected's content do not match actual's");
+            }
+        }
+
+        private class Product
+        {
+            public int Key { get; set; }
+            public string Name { get; set; }
+        }
+
+        private class IntEqualityComparer : IEqualityComparer<int>
+        {
+            public bool Equals(int x, int y)
+            {
+                return (x == y);
+            }
+            public int GetHashCode(int obj)
+            {
+                return obj;
+            }
+        }
+    }
+}
