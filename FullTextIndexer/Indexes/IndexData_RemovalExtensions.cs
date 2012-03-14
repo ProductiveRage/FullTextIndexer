45117560353f18b7ab444c7a6853733aebad864e
dan <dan@newmind.co.uk>
2012-03-14 22:43:15 +0000

    Split up IndexData into core functionality and supplementary extension methods

---
+++ FullTextIndexer/Indexes/IndexData_RemovalExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Lists;
+
+namespace FullTextIndexer.Indexes
+{
+    public static class IndexData_RemovalExtensions
+    {
+        /// <summary>
+        /// This will return a new IndexData instance without any data relating to keys identified by the removeIf filter
+        /// </summary>
+        public static IndexData<TKey> Remove<TKey>(this IndexData<TKey> index, Predicate<TKey> removeIf)
+        {
+            if (removeIf == null)
+                throw new ArgumentNullException("removeIf");
+
+            var dataNew = new Dictionary<string, NonNullImmutableList<WeightedEntry<TKey>>>(
+                index.TokenComparer
+            );
+            foreach (var token in index.GetAllTokens())
+            {
+                var matchesForToken = index.GetMatches(token);
+                var trimmedWeightedEntries = matchesForToken.Where(m => !removeIf(m.Key));
+                if (trimmedWeightedEntries.Any())
+                    dataNew.Add(token, trimmedWeightedEntries.ToNonNullImmutableList());
+            }
+
+            // Return a new instance containing the combined data
+            return new IndexData<TKey>(
+                new ImmutableDictionary<string, NonNullImmutableList<WeightedEntry<TKey>>>(
+                    dataNew,
+                    index.TokenComparer
+                ),
+                index.KeyComparer
+            );
+        }
+    }
+}
