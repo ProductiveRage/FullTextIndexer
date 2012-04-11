530bc1eddd300b441fbd09e9669fed1a056d1e1c
dan <742348+ProductiveRage@users.noreply.github.com>
2012-04-11 20:55:53 +0100

    Altered Constants Stopwords to handle multi-lingual content

--- FullTextIndexer/Constants.cs
+++ FullTextIndexer/Constants.cs
@@ -1,11 +1,23 @@
-﻿using Common.Lists;
+﻿using System;
+using Common.Lists;
 
 namespace FullTextIndexer
 {
     public static class Constants
     {
+        public static NonNullOrEmptyStringList GetStopWords(string languageCode)
+        {
+            if (string.IsNullOrWhiteSpace(languageCode))
+                throw new ArgumentException("Null/empty languageCode");
+
+            // Currently only support English stopwords
+            if (languageCode.Equals("en", StringComparison.InvariantCultureIgnoreCase))
+                return EnglishStopWords;
+            return new NonNullOrEmptyStringList();
+        }
+            
         // "Borrowed" these from Lucene.Net
-        public static NonNullOrEmptyStringList StopWords = new NonNullOrEmptyStringList(
+        private static NonNullOrEmptyStringList EnglishStopWords = new NonNullOrEmptyStringList(
             new[] { "a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "such", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with" }
         );
     }
