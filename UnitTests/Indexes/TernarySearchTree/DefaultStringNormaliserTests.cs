f581b4342822b2e5c0d05dd4b7e46fa70dae81d4
dan <742348+ProductiveRage@users.noreply.github.com>
2012-06-24 17:08:28 +0100

    Added a couple of unit tests for the DefaultStringNormaliser and EnglishPluarityStringNormaliser

---
+++ UnitTests/Indexes/TernarySearchTree/DefaultStringNormaliserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FullTextIndexer.Indexes.TernarySearchTree;
+using Xunit;
+
+namespace UnitTests.Indexes.TernarySearchTree
+{
+    public class DefaultStringNormaliserTests
+    {
+        [Fact]
+        public void NullWillThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                (new DefaultStringNormaliser()).GetNormalisedString(null);
+            });
+        }
+
+        [Fact]
+        public void CatIsLowerCased()
+        {
+            Test("Cat", "cat");
+        }
+
+        [Fact]
+        public void AcuteAccentRemovedOnMange()
+        {
+            Test("mangé", "mange");
+        }
+
+        private void Test(string valueToNormalise, string expected)
+        {
+            if (valueToNormalise == null)
+                throw new ArgumentNullException("valueToNormalise");
+            if (expected == null)
+                throw new ArgumentNullException("expected");
+
+            Assert.Equal(
+                expected,
+                (new DefaultStringNormaliser()).GetNormalisedString(valueToNormalise)
+            );
+        }
+    }
+}
---
+++ UnitTests/Indexes/TernarySearchTree/EnglishPluarityStringNormaliserTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FullTextIndexer.Indexes.TernarySearchTree;
+using Xunit;
+
+namespace UnitTests.Indexes.TernarySearchTree
+{
+    public class EnglishPluarityStringNormaliserTests
+    {
+        [Fact]
+        public void NullWillThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                (new EnglishPluralityStringNormaliser()).GetNormalisedString(null);
+            });
+        }
+
+        [Fact]
+        public void CatIsTransformedToCat_ses_es_s()
+        {
+            Test("Cat", "Cat[ses][es][s]");
+        }
+
+        [Fact]
+        public void CatsIsTransformedToCat_ses_es_s()
+        {
+            Test("Cats", "Cat[ses][es][s]");
+        }
+
+        private void Test(string valueToNormalise, string expected)
+        {
+            if (valueToNormalise == null)
+                throw new ArgumentNullException("valueToNormalise");
+            if (expected == null)
+                throw new ArgumentNullException("expected");
+
+            Assert.Equal(
+                expected,
+                (new EnglishPluralityStringNormaliser()).GetNormalisedString(valueToNormalise)
+            );
+        }
+    }
+}
