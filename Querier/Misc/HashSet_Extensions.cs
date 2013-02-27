17f45f23f658e3e357fb85ba5aeb9e185adc2638
dan <742348+ProductiveRage@users.noreply.github.com>
2013-02-27 23:31:46 +0000

    Added QueryTranslator implementation that will take an IQuerySegment (like that returned by the BreakPointCharacterAnalyser.Process method) and retrieve results (WeightedEntry instances) according to the structure and terms of the segment

---
+++ Querier/Misc/HashSet_Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Querier.Misc
+{
+	public static class HashSet_Extensions
+	{
+		public static void AddRange<T>(this HashSet<T> source, IEnumerable<T> values)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+			if (values == null)
+				throw new ArgumentNullException("values");
+
+			foreach (var value in values)
+				source.Add(value);
+		}
+	}
+}
