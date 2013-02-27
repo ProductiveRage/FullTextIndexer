b9ac4602738e1e56c7b6a0aa7b967b992e6b116e
dan <dan@newmind.co.uk>
2013-02-27 23:31:46 +0000

    Added QueryTranslator implementation that will take an IQuerySegment (like that returned by the BreakPointCharacterAnalyser.Process method) and retrieve results (WeightedEntry instances) according to the structure and terms of the segment

---
+++ Querier/QueryTranslators/IQueryTranslator.cs
@@ -0,0 +1,18 @@
+﻿using Common.Lists;
+using FullTextIndexer.Indexes;
+using Querier.QuerySegments;
+
+namespace Querier.QueryTranslators
+{
+	/// <summary>
+	/// This will retrieve results from index data that match the specified IQuerySegment implementation requirements
+	/// </summary>
+	public interface IQueryTranslator<TKey>
+	{
+		/// <summary>
+		/// This will never return null but may return an empty set if no matches could be made. An exception will be raised for a null querySegment
+		/// reference of if the request could otherwise not be satisfied (eg. unsupported IQuerySegment implementation)
+		/// </summary>
+		NonNullImmutableList<WeightedEntry<TKey>> GetMatches(IQuerySegment querySegment);
+	}
+}
---
+++ Querier/QueryTranslators/QueryTranslator.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Lists;
+using FullTextIndexer.Indexes;
+using Querier.Misc;
+using Querier.QuerySegments;
+
+namespace Querier.QueryTranslators
+{
+	/// <summary>
+	/// This will retrieve results from index data that match the specified IQuerySegment implementation requirements. Two index data sets are required; the
+	/// preciseMatchIndexData is used for retrieving results from PreciseMatchQuerySegment data while other data is retrieved from the standardMatchIndexData.
+	/// These two indexes should have the same source data (though likely different processing methods to build the index data) and both share the
+	/// same keyComparer as that specified in the QueryTranslator constructor. The matchCombiner is used to ensure that any set of query segments
+	/// returns only one WeightedEntry for any key. If a key appears multiple times in different nested query segments then the combination of
+	/// results for that key may occur multiple times - this allows, depending upon the MatchCombiner implementation, to give lower weight to
+	/// results the deeper nested that the originating query segments are (query segments are said to be nested if they are among the query
+	/// segments found in a CombiningQuerySegment).
+	/// </summary>
+	public class QueryTranslator<TKey> : IQueryTranslator<TKey>
+	{
+		private readonly CachingResultMatcher _standardMatcher;
+		private readonly CachingResultMatcher _preciseMatcher;
+		private readonly IEqualityComparer<TKey> _keyComparer;
+		private readonly MatchCombiner _matchCombiner;
+		public QueryTranslator(
+			IIndexData<TKey> standardMatchIndexData,
+			IIndexData<TKey> preciseMatchIndexData,
+			IEqualityComparer<TKey> keyComparer,
+			MatchCombiner matchCombiner)
+		{
+			if (standardMatchIndexData == null)
+				throw new ArgumentNullException("standardMatchIndexData");
+			if (preciseMatchIndexData == null)
+				throw new ArgumentNullException("preciseMatchIndexData");
+			if (keyComparer == null)
+				throw new ArgumentNullException("keyComparer");
+			if (matchCombiner == null)
+				throw new ArgumentNullException("matchCombiner");
+
+			_standardMatcher = new CachingResultMatcher(standardMatchIndexData);
+			_preciseMatcher = new CachingResultMatcher(preciseMatchIndexData);
+			_keyComparer = keyComparer;
+			_matchCombiner = matchCombiner;
+		}
+
+		/// <summary>
+		/// This will never be called with null or empty matchWeights or sourceQuerySegments lists. It must always return a value greater than zero.
+		/// The sourceQuerySegments set will contain all query segments in the current group - if this is a nested (bracketed) set of query terms
+		/// then the terms in that bracketed set will be included, not any parent segments. This data is included so that a lower weight may be
+		/// applied to nested query terms by dividing the combined weights by the number of segments in the set, for example.
+		/// </summary>
+		public delegate float MatchCombiner(ImmutableList<float> matchWeights, NonNullImmutableList<IQuerySegment> sourceQuerySegments);
+
+		/// <summary>
+		/// This will never return null but may return an empty set if no matches could be made. An exception will be raised for a null querySegment
+		/// reference of if the request could otherwise not be satisfied (eg. unsupported IQuerySegment implementation)
+		/// </summary>
+		public NonNullImmutableList<WeightedEntry<TKey>> GetMatches(IQuerySegment querySegment)
+		{
+			if (querySegment == null)
+				throw new ArgumentNullException("querySegment");
+
+			var combiningQuerySegment = querySegment as CombiningQuerySegment;
+			if (combiningQuerySegment != null)
+				return Reduce(combiningQuerySegment.Segments);
+
+			return Reduce(new NonNullImmutableList<IQuerySegment>(new IQuerySegment[] { querySegment }));
+		}
+
+		private NonNullImmutableList<WeightedEntry<TKey>> Reduce(NonNullImmutableList<IQuerySegment> querySegments)
+		{
+			if (querySegments == null)
+				throw new ArgumentNullException("querySegments");
+
+			HashSet<TKey> compulsoryKeys = null;
+			var exclusionKeys = new HashSet<TKey>(_keyComparer);
+			var allInclusiveWeighedMatches = new List<WeightedEntry<TKey>>();
+			foreach (var querySegment in querySegments)
+			{
+				var combiningQuerySegment = querySegment as CombiningQuerySegment;
+				if (combiningQuerySegment != null)
+				{
+					allInclusiveWeighedMatches.AddRange(
+						Reduce(combiningQuerySegment.Segments)
+					);
+					continue;
+				}
+
+				var compulsoryQuerySegment = querySegment as CompulsoryQuerySegment;
+				if (compulsoryQuerySegment != null)
+				{
+					if (compulsoryKeys == null)
+						compulsoryKeys = new HashSet<TKey>(_keyComparer);
+					var compulsoryQuerySegmentKeys = GetMatches(compulsoryQuerySegment.Segment);
+					compulsoryKeys.AddRange(compulsoryQuerySegmentKeys.Select(e => e.Key));
+					allInclusiveWeighedMatches.AddRange(compulsoryQuerySegmentKeys);
+					continue;
+				}
+
+				var excludingQuerySegment = querySegment as ExcludingQuerySegment;
+				if (excludingQuerySegment != null)
+				{
+					exclusionKeys.AddRange(
+						GetMatches(excludingQuerySegment.Segment).Select(e => e.Key)
+					);
+					continue;
+				}
+
+				if (querySegment is NoMatchContentQuerySegment)
+					continue;
+
+				var preciseMatchQuerySegment = querySegment as PreciseMatchQuerySegment;
+				if (preciseMatchQuerySegment != null)
+				{
+					allInclusiveWeighedMatches.AddRange(
+						_preciseMatcher.GetMatches(preciseMatchQuerySegment.Value)
+					);
+					continue;
+				}
+
+				var standardMatchQuerySegment = querySegment as StandardMatchQuerySegment;
+				if (standardMatchQuerySegment != null)
+				{
+					allInclusiveWeighedMatches.AddRange(
+						_standardMatcher.GetMatches(standardMatchQuerySegment.Value)
+					);
+					continue;
+				}
+
+				throw new NotSupportedException("Unsupported IQuerySegment type: " + querySegment.GetType());
+			}
+
+			// Filter the matches to respect the exclusionKeys (remove them entirely) and compulsoryKeys (remove any not specified) data
+			var combinedKeys = allInclusiveWeighedMatches.Where(e => !exclusionKeys.Contains(e.Key));
+			if (compulsoryKeys != null)
+				combinedKeys = combinedKeys.Where(e => compulsoryKeys.Contains(e.Key));
+			
+			// Group the data so that no key appears more than once by combining the match weights
+			return combinedKeys
+				.GroupBy(e => e.Key, _keyComparer)
+				.Select(group => new WeightedEntry<TKey>(group.Key, _matchCombiner(group.Select(e => e.Weight).ToImmutableList(), querySegments)))
+				.ToNonNullImmutableList();
+		}
+
+		private class CachingResultMatcher
+		{
+			private readonly IIndexData<TKey> _index;
+			private readonly Dictionary<string, NonNullImmutableList<WeightedEntry<TKey>>> _cache;
+			public CachingResultMatcher(IIndexData<TKey> index)
+			{
+				if (index == null)
+					throw new ArgumentNullException("index");
+
+				_index = index;
+				_cache = new Dictionary<string, NonNullImmutableList<WeightedEntry<TKey>>>();
+			}
+
+			public NonNullImmutableList<WeightedEntry<TKey>> GetMatches(string source)
+			{
+				if (source == null)
+					throw new ArgumentNullException("source");
+
+				lock (_cache)
+				{
+					NonNullImmutableList<WeightedEntry<TKey>> cachedData;
+					if (_cache.TryGetValue(source, out cachedData))
+						return cachedData;
+				}
+				
+				var data = _index.GetMatches(source);
+				lock (_cache)
+				{
+					if (!_cache.ContainsKey(source))
+						_cache.Add(source, data);
+				}
+				return data;
+			}
+		}
+	}
+}
