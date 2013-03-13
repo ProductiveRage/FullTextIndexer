27537121148e87fa35e8af39acb9d1968950eb0e
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-13 20:35:21 +0000

    Added IQuerier and Querier interface/class that tie together the QueryAnalysers and QueryTranslators into a single GetMatches method

---
+++ Querier/IQuerier.cs
@@ -0,0 +1,13 @@
+﻿using FullTextIndexer.Common.Lists;
+using FullTextIndexer.Core.Indexes;
+
+namespace FullTextIndexer.Querier
+{
+	public interface IQuerier<TSource, TKey>
+	{
+		/// <summary>
+		/// This will never return null. It will throw an exception for a null or blank searchTerm.
+		/// </summary>
+		NonNullImmutableList<WeightedEntry<TKey>> GetMatches(string searchTerm);
+	}
+}
