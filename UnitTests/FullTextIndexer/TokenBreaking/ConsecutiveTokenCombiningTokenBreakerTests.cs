44929230295c6601d1055c87465bf540d190a4c4
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-16 16:51:13 +0000

    Extended the WeightAdjustingToken such that index and length of the content in the source are recorded (if stored in the index data as well then this should simplify result highlighting) - Updated xUnit reference since newer versions make IEnumerable comparisons easier

---
+++ UnitTests/FullTextIndexer/TokenBreaking/ConsecutiveTokenCombiningTokenBreakerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using FullTextIndexer.Common.Lists;
+using FullTextIndexer.Core.TokenBreaking;
+using Xunit;
+
+namespace UnitTests.FullTextIndexer.TokenBreaking
+{
+	public class ConsecutiveTokenCombiningTokenBreakerTests
+	{
+		/// <summary>
+		/// The length of the combined content should not just be the sum of the lengths of the combined tokens since this wouldn't take into account any
+		/// breaking characters (such as whitespace, depending upon the wrapped token breaker) between the tokens
+		/// </summary>
+		[Fact]
+		public void CombinedTokensShouldTakeTheIndexOfTheFirstTokenAndTheLengthOfTheEntireSourceSegment()
+		{
+			var content = "one two three";
+			var whiteSpaceBrokenTokens = new NonNullImmutableList<WeightAdjustingToken>(new[]
+			{
+				new WeightAdjustingToken("one", 0, 3, 1),
+				new WeightAdjustingToken("two", 4, 3, 1),
+				new WeightAdjustingToken("three", 8, 5, 1)
+			});
+
+			var expected = new NonNullImmutableList<WeightAdjustingToken>(new[]
+			{
+				new WeightAdjustingToken("one", 0, 3, 1),
+				new WeightAdjustingToken("two", 4, 3, 1),
+				new WeightAdjustingToken("three", 8, 5, 1),
+				new WeightAdjustingToken("one two", 0, 7, 1),
+				new WeightAdjustingToken("two three", 4, 9, 1),
+				new WeightAdjustingToken("one two three", 0, 13, 1)
+			});
+			
+			Assert.Equal<WeightAdjustingToken>(
+				expected,
+				new ConsecutiveTokenCombiningTokenBreaker(
+					new FixedContentTokenBreaker(content, whiteSpaceBrokenTokens),
+					3,
+					weightMultipliersOfCombinedTokens => weightMultipliersOfCombinedTokens.Average()
+				).Break(content),
+				new WeightAdjustingTokenComparer()
+			);
+		}
+
+		private class FixedContentTokenBreaker : ITokenBreaker
+		{
+			private readonly string _expectedValue;
+			private readonly NonNullImmutableList<WeightAdjustingToken> _results;
+			public FixedContentTokenBreaker(string expectedValue, NonNullImmutableList<WeightAdjustingToken> results)
+			{
+				if (expectedValue == null)
+					throw new ArgumentNullException("expectedValue");
+				if (results == null)
+					throw new ArgumentNullException("results");
+
+				_expectedValue = expectedValue;
+				_results = results;
+
+			}
+
+			public NonNullImmutableList<WeightAdjustingToken> Break(string value)
+			{
+				if (value == null)
+					throw new ArgumentNullException("value");
+				
+				if (value != _expectedValue)
+					throw new ArgumentException("This FixedContentTokenBreaker instance is only configured to Break the value: \"" + _expectedValue + "\"");
+
+				return _results;
+			}
+		}
+	}
+}
---
+++ UnitTests/FullTextIndexer/TokenBreaking/WeightAdjustingTokenComparer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using FullTextIndexer.Core.TokenBreaking;
+
+namespace UnitTests.FullTextIndexer.TokenBreaking
+{
+	public class WeightAdjustingTokenComparer : IEqualityComparer<WeightAdjustingToken>
+	{
+		public bool Equals(WeightAdjustingToken x, WeightAdjustingToken y)
+		{
+			if (x == null)
+				throw new ArgumentNullException("x");
+			if (y == null)
+				throw new ArgumentNullException("y");
+
+			return (
+				(x.Token == y.Token) &&
+				(x.SourceIndex == y.SourceIndex) &&
+				(x.SourceTokenLength == y.SourceTokenLength) &&
+				(x.WeightMultiplier == y.WeightMultiplier)
+			);
+		}
+
+		public int GetHashCode(WeightAdjustingToken obj)
+		{
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+
+			return 0;
+		}
+	}
+}
