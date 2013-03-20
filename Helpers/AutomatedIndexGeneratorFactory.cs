3dd3dda6be5b939fdfddeea60292eac86cb45fa4
dan <742348+ProductiveRage@users.noreply.github.com>
2013-03-20 00:26:53 +0000

    Removed a no-longer-required "using"

--- Helpers/AutomatedIndexGeneratorFactory.cs
+++ Helpers/AutomatedIndexGeneratorFactory.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using FullTextIndexer.Common.Lists;
 using FullTextIndexer.Common.Logging;
 using FullTextIndexer.Core.Indexes.TernarySearchTree;
